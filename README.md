7ed648ff2b89fec92e787ca5e92573b13a07e237
Ayman Talat <aymantalat2@gmail.com>
2024-06-15 12:29:58 +0300

    add heading element, add resize grid button and implement the sketch feature (using mouse event)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Etch-a-Sketch
 
 ### Project Description
-This is the fourth project in the foundations course (JavaScript section) of the Odin project curriculum. The project is a browser version of the Etch-A-Sketch game.
+This is the fourth project in the foundations course (JavaScript section) of the Odin project curriculum. The project is a browser version of the Etch-A-Sketch game. 
 
 ### Learned Skills
 - HTML
--- scripts.js
+++ scripts.js
@@ -1,13 +1,64 @@
-const GRID_SIZE = 16;
+const GRID_SIZE = 600;
+const BORDER_STYLE = "2px solid black";
 const container = document.querySelector(".container");
+const squaresNumBtn = document.querySelector(".squares-num-btn");
 
-for (let i = 0; i < GRID_SIZE; i++) {
-  const rowElem = document.createElement("div");
-  rowElem.classList.add("row");
-  container.appendChild(rowElem);
-  for (let j = 0; j < GRID_SIZE; j++) {
-    const squareElem = document.createElement("div");
-    squareElem.classList.add("square");
-    rowElem.appendChild(squareElem);
+function getSquaresNum() {
+  squaresNum = Number(prompt("Enter the number of squares per row (max: 100): "));
+  while (squaresNum > 100 || squaresNum < 1 || Number.isNaN(squaresNum)) {
+    squaresNum = Number(prompt("Enter the number of squares per row (max: 100): "));
+  }
+  return squaresNum;
+}
+
+function removeGrid() {
+  let rowElem = container.lastElementChild;
+  while (rowElem) {
+    container.removeChild(rowElem);
+    rowElem = container.lastElementChild;
   }
 }
+
+function createGrid(squaresNum) {
+  for (let i = 0; i < squaresNum; i++) {
+    const rowElem = document.createElement("div");
+    rowElem.classList.add("row");
+    container.appendChild(rowElem);
+    for (let j = 0; j < squaresNum; j++) {
+      const squareElem = document.createElement("div");
+      squareElem.classList.add("square");
+      const squareSize = GRID_SIZE / squaresNum;
+      squareElem.style.minHeight = `${squareSize}px`;
+      squareElem.style.minWidth = `${squareSize}px`;
+      if (i === 0) {
+        squareElem.style.borderTop = BORDER_STYLE;
+      }
+      if (i === squaresNum - 1) {
+        squareElem.style.borderBottom = BORDER_STYLE;
+      }
+      rowElem.appendChild(squareElem);
+    }
+    rowElem.firstElementChild.style.borderLeft = BORDER_STYLE;
+    rowElem.lastElementChild.style.borderRight = BORDER_STYLE;
+  }
+}
+
+// Grid resize event
+squaresNumBtn.addEventListener("click", () => {
+  squaresNum = getSquaresNum();
+  removeGrid();
+  createGrid(squaresNum);
+});
+
+// hover event
+container.addEventListener("mouseover", (event) => {
+  console.log("hover");
+  event.target.style.backgroundColor = "#adb5bd";
+});
+
+
+// Init the grid with default number of squares (16x16)
+createGrid(16);
+
+
+
